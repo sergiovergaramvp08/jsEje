d3eae2a3ad0b37beed00c9e16a9a3773c469f168
sergiovergaramvp08 <sergiomvp08@gmail.com>
2025-03-31 15:20:34 -0300

    version 2

---
+++ ejercio 10/script.js
@@ -0,0 +1,13 @@
+function boton(){
+    var dado1, dado2, result, array = [0,0,0,0,0,0,0,0,0,0,0]
+    for(i=0; i<36000; i++){
+        dado1 = Math.floor(Math.random() * 7)
+        dado2 = Math.floor(Math.random() * 7)
+        result = dado1 + dado2
+        array[result-2]++
+    }
+    console.log(array)
+    for(r=2; r<=12; r++){
+        console.log(r+":"+array[r-2])
+    }
+}
