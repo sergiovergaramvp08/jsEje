f6f897e07772d72d6ff7c8626285ce06c5db28ff
sergiovergaramvp08 <sergiomvp08@gmail.com>
2025-04-13 22:18:13 -0300

    Update script.js

--- ejercicio5/script.js
+++ ejercicio5/script.js
@@ -1,18 +1,10 @@
-function boton() {
-    var numStr = prompt("NUMERO:");
-    var num = parseInt(numStr);
-    var x = 0;
-    var y = num;
-  
-    
-    if (num === 0) {
-      console.log("El factorial de 0 es: 1");
-      return;
+function boton(){
+    var num = parseInt(prompt("COLOCA UN NUMERO"))
+    var x, y = num
+
+    for(let n=(num-1); n>0; n--){
+        x = y * n
+        y = x
     }
-  
-    for (let n = (num - 1); n > 0; n--) {
-      x = y * n;
-      y = x;
-    }
-    console.log(y);
-  }
+    console.log(y)
+}
