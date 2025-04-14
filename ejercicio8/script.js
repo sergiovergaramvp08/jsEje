458b50f4ae615cde0880b032af84b950b0d1eb49
sergiovergaramvp08 <sergiomvp08@gmail.com>
2025-04-13 23:36:28 -0300

    Update script.js

--- ejercicio8/script.js
+++ ejercicio8/script.js
@@ -1,25 +1,20 @@
 function boton() {
-    let text = 'La ruta nos aporto otro paso natural';
-    
-    let arr = text.replaceAll(' ', '').split('');
+    let text = 'la ruta nos aporto otro paso natural'
+    let arr = text.replaceAll(' ', '').split('')
+    let aciertos = 0, i=0, l = arr.length-1
 
-    let i = 0;           
-    let j = arr.length - 1; 
-    let isPalindrome = true;  
-
-    while (i < j) {
-        if (arr[i] !== arr[j]) {
-            isPalindrome = false;  
-            break;  
+    while(i<arr.length-1){
+        if(arr[i] == arr[l]){
+            aciertos++
         }
-        i++;
-        j--;
+        i++
+        l--
     }
 
-    
-    if (isPalindrome) {
-        console.log("Es un palíndromo");
-    } else {
-        console.log("No es un palíndromo");
+    if(aciertos == arr.length-1){
+        console.log("es palindromo")
+    }
+    else{
+        console.log("no es palindromo")
     }
 }
