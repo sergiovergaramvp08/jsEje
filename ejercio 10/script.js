c79b15d72af20181544bd2cbbb555ac80aab8019
sergiovergaramvp08 <sergiomvp08@gmail.com>
2025-04-14 00:00:51 -0300

    Update script.js

--- ejercio 10/script.js
+++ ejercio 10/script.js
@@ -1,8 +1,8 @@
 function boton(){
     var dado1, dado2, result, array = [0,0,0,0,0,0,0,0,0,0,0]
     for(i=0; i<36000; i++){
-        dado1 = Math.floor(Math.random() * 7)
-        dado2 = Math.floor(Math.random() * 7)
+        dado1 = parseInt(Math.random() * 7)
+        dado2 = parseInt(Math.random() * 7)
         result = dado1 + dado2
         array[result-2]++
     }
